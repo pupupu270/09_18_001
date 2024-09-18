6b08135a8e0c757b56343502022ab05e810da0b0
pupupu270 <lycee0@yahoo.co.jp>
2024-09-18 18:56:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 
 
+
+require("dotenv").config();
+
+const  {Client} = require("pg");
+
+const sql= new Client({
+connectionString:process.env.CONNECTION_STRING
+});
+sql.connect();
+
+sql.end();
+
+
+
+
 let websocket = require("ws");
 let server = new websocket.Server({port: 2001});
 
