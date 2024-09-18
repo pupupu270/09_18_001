c1442c6fd6d255b419163e28b58a952028500408
pupupu270 <lycee0@yahoo.co.jp>
2024-09-18 21:44:24 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
 
 
 
-require("dotenv").config();
-
-const  {Client} = require("pg");
-
-const sql= new Client({
-connectionString:process.env.CONNECTION_STRING
-});
-sql.connect();
-
-sql.end();
-
-
-
 
 let websocket = require("ws");
 let server = new websocket.Server({port: 2001});
-
-let client = 0;
 
 
 
@@ -26,24 +11,7 @@ let client = 0;
 server.on("connection", (socket) => {
 
     console.log("connection");
-    let json = {
-        connection_id: client
-    };
-    socket.send(JSON.stringify(json));
-    client++;
-    
-
-
-
-    socket.on("message", ms => {
-        let a = JSON.parse(ms.toString());
-        if (a.message != "")
-        {
-            server.clients.forEach((client) => {     
-                client.send(JSON.stringify(a));
-            });
-        }
-    });
+   
 });
 
 
